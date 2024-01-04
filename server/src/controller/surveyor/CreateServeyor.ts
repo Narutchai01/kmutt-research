4fd78a7b616feafd173cdcf52017115583d3d082
Narutchai <koonfluk2015@gmail.com>
2024-01-04 21:49:28 +0700

    update

--- server/src/controller/surveyor/CreateServeyor.ts
+++ server/src/controller/surveyor/CreateServeyor.ts
@@ -1,22 +1,29 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 import { conn } from "../../server";
 import { hashPassword } from "../../utils/ManagePassWord";
-import { v4 as uuidv4 } from 'uuid';
-
+import { v4 as uuidv4 } from "uuid";
 
 export const CreateSurveyor = async (req: Request, res: Response) => {
     try {
-        const { First_name, Last_name, email, PassWord, Phone_number } = req.body;
-        const sql = `INSERT INTO surveyor (SurveyorID, First_name, Last_name, Phone_number , email ,PassWord) VALUES (?, ?, ?, ?, ?, ?)`;
+        const { First_name, Last_name, Email, PassWord, Phone_number, Birth_date } =
+            req.body;
+        const sql = `INSERT INTO Surveyor ( First_name, Last_name, Birth_date , Phone_number ,Email , PassWord) VALUES ( ?, ?, ?, ?, ?, ?)`;
         const dataSurveyor = {
-            SurveyorID: uuidv4(),
             First_name,
             Last_name,
-            email,
+            Email,
+            Birth_date,
             PassWord: await hashPassword(PassWord),
-            Phone_number
+            Phone_number,
         };
-        await conn?.query(sql, [dataSurveyor.SurveyorID, dataSurveyor.First_name, dataSurveyor.Last_name, dataSurveyor.Phone_number, dataSurveyor.email, dataSurveyor.PassWord]);
+        await conn?.query(sql, [
+            dataSurveyor.First_name,
+            dataSurveyor.Last_name,
+            dataSurveyor.Birth_date,
+            dataSurveyor.Phone_number,
+            dataSurveyor.Email,
+            dataSurveyor.PassWord,
+        ]);
         res.status(200).json({ message: "Create Surveyor Success" });
     } catch (error) {
         console.log(error);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { Config } from "./lib/config";
 import mysql from "mysql2/promise";
-import  SurveyorRouter  from "./routers/Surveyor";
-import CarRouter  from "./routers/Cars";
+import multer from "multer";
+
+import SurveyorRouter from "./routers/Surveyor";
+import CarRouter from "./routers/Cars";
 import CasesRouter from "./routers/Cases";
 
 const app = express();
@@ -10,28 +12,33 @@ const PORT = Config.PORT;
 
 app.use(express.json());
 
-export let conn : mysql.Connection | null = null;
+export let conn: mysql.Connection | null = null;
 const Connect = async () => {
-    conn = await mysql.createConnection({
-        host: Config.DB_HOST,
-        user: Config.DB_USER,
-        password: Config.DB_PASSWORD,
-        database: Config.DB_NAME,
-        port: Config.DB_PORT,
-    });
-    console.log("Database Connected");
-    
+  conn = await mysql.createConnection({
+    host: Config.DB_HOST,
+    user: Config.DB_USER,
+    password: Config.DB_PASSWORD,
+    database: Config.DB_NAME,
+    port: Config.DB_PORT,
+  });
+  console.log("Database Connected");
 };
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const multerMid = multer({
+  storage: multer.memoryStorage(),
 });
 
-app.use('/api/surveyor', SurveyorRouter);
-app.use('/api/cases', CasesRouter);
-app.use('/api/cars', CarRouter);
+app.use(multerMid.array("file"));
 
-app.listen(PORT, async() => { 
-    await Connect();
-    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api/surveyor", SurveyorRouter);
+app.use("/api/cases", CasesRouter);
+app.use("/api/cars", CarRouter);
+
+app.listen(PORT, async () => {
+  await Connect();
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
