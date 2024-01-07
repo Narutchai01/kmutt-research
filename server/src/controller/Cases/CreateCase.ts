82aea9579ff6ec910f20de7cfefbd5b1575de92f
Narutchai <koonfluk2015@gmail.com>
2024-01-07 13:51:26 +0700

    update

--- server/src/controller/Cases/CreateCase.ts
+++ server/src/controller/Cases/CreateCase.ts
@@ -10,6 +10,7 @@ export const CreateCase = async (req: Request, res: Response) => {
     const decoded: any = jwt.verify(token, secert);
     const CaseID = uuidv4();
     const { CarID, Province, Description } = req.body;
+    const Image = req.files;
     const addCase = `INSERT INTO Cases (CaseID, SurveyorID, CarID, Province, Description) VALUES (?,?,?,?,?)`;
     const date = new Date();
     const Date_opened = `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2,'0')}-${(date.getDate()).toString().padStart(2,'0')}`;
@@ -21,15 +22,21 @@ export const CreateCase = async (req: Request, res: Response) => {
       Date_opened,
       Description,
     };
-    await conn?.query(addCase, [
-      DataCase.CaseID,
-      DataCase.SurveyorID,
-      DataCase.CarID,
-      DataCase.Province,
-      DataCase.Date_opened,
-      DataCase.Description,
-    ]);
-    res.status(200).json({ message: "Create Case Success", data: DataCase });
+    // await conn?.query(addCase, [
+    //   DataCase.CaseID,
+    //   DataCase.SurveyorID,
+    //   DataCase.CarID,
+    //   DataCase.Province,
+    //   DataCase.Date_opened,
+    //   DataCase.Description,
+    // ]);
+
+    const Images = await Promise.all(
+      (Image as any[]).map(async (image: any) => {
+        return image.buffer.toString("base64");
+      })
+    );
+    res.status(200).json({ message: "Create Case Success", data: DataCase , Images});
   } catch (error) {
     console.log(error);
   }
--- server/src/controller/surveyor/CreateServeyor.ts
+++ server/src/controller/surveyor/CreateServeyor.ts
@@ -3,28 +3,27 @@ import { conn } from "../../server";
 import { hashPassword } from "../../utils/ManagePassWord";
 
 export const CreateSurveyor = async (req: Request, res: Response) => {
-    try {
-        const { First_name, Last_name, Email, PassWord, Phone_number, Birth_date } =
-            req.body;
-        const sql = `INSERT INTO Surveyor ( First_name, Last_name, Birth_date , Phone_number ,Email , PassWord) VALUES ( ?, ?, ?, ?, ?, ?)`;
-        const dataSurveyor = {
-            First_name,
-            Last_name,
-            Email,
-            Birth_date,
-            PassWord: await hashPassword(PassWord),
-            Phone_number,
-        };
-        await conn?.query(sql, [
-            dataSurveyor.First_name,
-            dataSurveyor.Last_name,
-            dataSurveyor.Birth_date,
-            dataSurveyor.Phone_number,
-            dataSurveyor.Email,
-            dataSurveyor.PassWord,
-        ]);
-        res.status(200).json({ message: "Create Surveyor Success" });
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const { First_name, Last_name, Email, PassWord, Phone_number, Birth_date } =req.body;
+    const sql = `INSERT INTO Surveyor ( First_name, Last_name, Birth_date , Phone_number ,Email , PassWord) VALUES ( ?, ?, ?, ?, ?, ?)`;
+    const dataSurveyor = {
+      First_name,
+      Last_name,
+      Email,
+      Birth_date,
+      PassWord: await hashPassword(PassWord),
+      Phone_number,
+    };
+    await conn?.query(sql, [
+        dataSurveyor.First_name,
+        dataSurveyor.Last_name,
+        dataSurveyor.Birth_date,
+        dataSurveyor.Phone_number,
+        dataSurveyor.Email,
+        dataSurveyor.PassWord,
+    ]);
+    res.status(200).json(Image);
+  } catch (error) {
+    console.log(error);
+  }
 };
