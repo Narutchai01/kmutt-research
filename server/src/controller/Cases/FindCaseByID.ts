ad7a57af924130d81a038cb2e484b2227ad760ae
Narutchai <koonfluk2015@gmail.com>
2024-01-10 18:04:05 +0700

    update FindecaseBySurveyorID

---
+++ server/src/controller/Cases/FindCaseByID.ts
@@ -0,0 +1,17 @@
+import { Request,Response } from "express";
+import { conn } from "../../server";
+import jwt from "jsonwebtoken";
+
+
+export const findCaseBySurveyorID = async (req:Request,res:Response) => {
+    try {
+        const token = req.cookies.token;
+        const secert = process.env.JWT_SECRET!;
+        const decoded:any= jwt.verify(token, secert) ;
+        const sql = `SELECT * FROM Cases WHERE SurveyorID = ?`;
+        const result:any = await conn?.query(sql,[decoded.ID]);
+        res.status(200).send(result[0]);
+    } catch (error) {
+        console.log(error);
+    }
+};
