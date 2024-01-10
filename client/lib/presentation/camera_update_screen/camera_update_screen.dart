c2f0e9da6807660573ca9cc3301fd0ff8c67ad39
CosmosDever <aprommhgot@gmail.com>
2024-01-10 14:44:54 +0700

    camera_init_update

--- client/lib/presentation/camera_update_screen/camera_update_screen.dart
+++ client/lib/presentation/camera_update_screen/camera_update_screen.dart
@@ -1,66 +1,3 @@
-// import 'package:client/core/app_export.dart';
-// import 'package:flutter/material.dart';
-
-// class CameraUpdateScreen extends StatelessWidget {
-//   const CameraUpdateScreen({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       child: Scaffold(
-//         body: Stack(
-//           children: [
-//             Positioned.fill(
-//               child: Container(
-//                 // This container can represent the camera viewfinder or viewport
-//                 color: Colors.black, // Example color, adjust as needed
-//               ),
-//             ),
-//             Positioned(
-//               bottom: 0,
-//               left: 0,
-//               right: 0,
-//               child: Padding(
-//                 padding: EdgeInsets.symmetric(horizontal: 15.h),
-//                 child: Row(
-//                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                   children: [
-//                     _buildCustomImageView(
-//                       imagePath: ImageConstant.imgTelevision,
-//                       onTap: () => onTapImgTelevision(context),
-//                     ),
-//                     _buildCustomImageView(
-//                       imagePath: ImageConstant.imgCamera,
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//             // Include _buildCameraUpdateRow at the top if necessary
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-// Widget _buildCustomImageView(
-//     {required String imagePath, VoidCallback? onTap}) {
-//   return GestureDetector(
-//     onTap: onTap,
-//     child: CustomImageView(
-//       imagePath: imagePath,
-//       height: 59.v,
-//       width: 59.h,
-//     ),
-//   );
-// }
-
-//   onTapImgTelevision(BuildContext context) {
-//     Navigator.pushNamed(context, AppRoutes.imagepickerUpdateScreen);
-//   }
-
-//   // Include _buildCameraUpdateRow and other methods if necessary
-// }
 import 'package:camera/camera.dart';
 import 'package:client/presentation/camera_update_screen/camera_previwe.dart';
 import 'package:flutter/material.dart';
@@ -77,6 +14,7 @@ class CameraUpdateScreen extends StatefulWidget {
 class _CameraUpdateScreenState extends State<CameraUpdateScreen> {
   late CameraController _controller;
   late List<CameraDescription> cameras;
+  bool _isCameraInitialized = false;
 
   @override
   void initState() {
@@ -85,13 +23,19 @@ class _CameraUpdateScreenState extends State<CameraUpdateScreen> {
   }
 
   Future<void> _initializeCamera() async {
-    cameras = await availableCameras();
-    _controller = CameraController(cameras[0], ResolutionPreset.max);
+    try {
+      cameras = await availableCameras();
+      _controller = CameraController(cameras[0], ResolutionPreset.max);
 
-    // Initialize the camera controller
-    await _controller.initialize();
-    if (mounted) {
-      setState(() {});
+      // Initialize the camera controller
+      await _controller.initialize();
+      if (mounted) {
+        setState(() {
+          _isCameraInitialized = true;
+        });
+      }
+    } catch (e) {
+      print('Error initializing camera: $e');
     }
   }
 
@@ -117,102 +61,59 @@ class _CameraUpdateScreenState extends State<CameraUpdateScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: double.infinity,
-            child: CameraPreview(_controller),
-          ),
-          Positioned(
-            bottom: 0,
-            left: -125,
-            right: 0,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 100.h),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildCustomImageView(
-                    imagePath: ImageConstant.imgTelevision,
-                    onTap: () => _pickImage(),
+      body: _isCameraInitialized
+          ? Stack(
+              children: [
+                Container(
+                  height: double.infinity,
+                  child: CameraPreview(_controller),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: -125,
+                  right: 0,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 100.h),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        _buildCustomImageView(
+                          imagePath: ImageConstant.imgTelevision,
+                          onTap: () => _pickImage(),
+                        ),
+                        _buildCustomImageView(
+                          imagePath: ImageConstant.imgCamera,
+                          onTap: () async {
+                            if (!_controller.value.isInitialized) {
+                              return null;
+                            }
+                            if (_controller.value.isTakingPicture) {
+                              return null;
+                            }
+                            try {
+                              await _controller.setFlashMode(FlashMode.auto);
+                              XFile file = await _controller.takePicture();
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => ImagePreview(file),
+                                ),
+                              );
+                            } on CameraException catch (e) {
+                              debugPrint("Error while taking picture");
+                              return null;
+                            }
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                  _buildCustomImageView(
-                    imagePath: ImageConstant.imgCamera,
-                    onTap: () async {
-                      if (!_controller.value.isInitialized) {
-                        return null;
-                      }
-                      if (_controller.value.isTakingPicture) {
-                        return null;
-                      }
-                      try {
-                        await _controller.setFlashMode(FlashMode.auto);
-                        XFile file = await _controller.takePicture();
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ImagePreview(file),
-                          ),
-                        );
-                      } on CameraException catch (e) {
-                        debugPrint("Error while taking picture");
-                        return null;
-                      }
-                    },
-                  ),
-                ],
-              ),
+                ),
+              ],
+            )
+          : Center(
+              child: CircularProgressIndicator(),
             ),
-          ),
-          // Row(
-          //   mainAxisAlignment: MainAxisAlignment.end,
-          //   crossAxisAlignment: CrossAxisAlignment.center,
-          //   children: [
-          //     Center(
-          //       child: Container(
-          //         margin: EdgeInsets.all(20.0),
-          //         child: MaterialButton(
-          //           onPressed: () async {
-          //             if (!_controller.value.isInitialized) {
-          //               return null;
-          //             }
-          //             if (_controller.value.isTakingPicture) {
-          //               return null;
-          //             }
-          //             try {
-          //               await _controller.setFlashMode(FlashMode.auto);
-          //               XFile file = await _controller.takePicture();
-          //               Navigator.push(
-          //                 context,
-          //                 MaterialPageRoute(
-          //                   builder: (context) => ImagePreview(file),
-          //                 ),
-          //               );
-          //             } on CameraException catch (e) {
-          //               debugPrint("Error while taking picture");
-          //               return null;
-          //             }
-          //           },
-          //           color: Colors.white,
-          //           child: Text("Take a picture"),
-          //         ),
-          //       ),
-          //     ),
-          //     Center(
-          //       child: Container(
-          //         margin: EdgeInsets.all(20.0),
-          //         child: MaterialButton(
-
-          //           onPressed: _pickImage,
-          //           color: Colors.white,
-          //           child: Text("Pick Image from Gallery"),
-          //         ),
-          //       ),
-          //     ),
-          //   ],
-          // )
-        ],
-      ),
     );
   }
 
