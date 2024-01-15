678569b114b977e26fd7e3f2965defbf3623459e
Narutchai <koonfluk2015@gmail.com>
2024-01-15 23:19:59 +0700

    finish ChangePassword

---
+++ server/src/controller/surveyor/ChangePassWord.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { conn } from "../../server";
+import { hashPassword, comparePassword } from "../../utils/ManagePassWord";
+import jwt from "jsonwebtoken";
+
+export const ChangePassWord = async (req: Request, res: Response) => {
+    try {
+        const { oldPassword, newPassword } = req.body;
+        const secert = process.env.JWT_SECRET!;
+        const { token } = req.params;
+        const decode:any = await jwt.verify(token, secert);
+        const findsurveyorSQL = `SELECT * FROM Surveyor WHERE SurveyorID = ?`;
+        const updatePasswordSQL = `UPDATE Surveyor SET Password = ? WHERE SurveyorID = ?`;
+        const surveyor:any =  await conn?.query(findsurveyorSQL, [decode.ID]);
+        if (!surveyor) {
+            res.status(400).json({ message: "Surveyor not found" });
+            return;
+        }
+        const isMatch = await comparePassword(oldPassword, surveyor[0][0].Password);
+        if (!isMatch) {
+            res.status(400).json({ message: "Old password is incorrect" });
+            return;
+        }
+        const hash = await hashPassword(newPassword);
+        await conn?.query(updatePasswordSQL, [hash, decode.ID]);
+        res.status(200).json({ message: "Change password success" });
+    } catch (error) {
+        console.log(error);
+    }
+};
