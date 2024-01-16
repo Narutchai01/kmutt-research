82171f08c69af588f16c15429279537d8d240832
twentytwo44 <95289695+Twentytwo44@users.noreply.github.com>
2024-01-16 17:46:05 +0700

    search function

--- client/lib/presentation/search_update_page/search_update_page.dart
+++ client/lib/presentation/search_update_page/search_update_page.dart
@@ -1,120 +1,133 @@
-import 'package:client/presentation/data_1_update_screen/data_1_update_screen.dart';
+// import 'dart:js';
+
+// import 'package:client/presentation/data_1_update_screen/data_1_update_screen.dart';
 import 'package:dio/dio.dart';
-import '../search_update_page/widgets/userlist_item_widget.dart';
-import 'package:client/core/app_export.dart';
-import 'package:client/widgets/custom_search_view.dart';
+// import '../search_update_page/widgets/userlist_item_widget.dart';
+// import 'package:client/core/app_export.dart';
+// import 'package:client/widgets/custom_search_view.dart';
 import 'package:flutter/material.dart';
+import 'package:client/presentation/model/search_model.dart';
+
+List<SearchModel> searchModels = []; // Declare searchModels list globally
 
 // ignore_for_file: must_be_immutable
-class SearchUpdatePage extends StatelessWidget {
+class SearchUpdatePage extends StatefulWidget {
   SearchUpdatePage({Key? key}) : super(key: key);
 
-  TextEditingController searchController = TextEditingController();
+  @override
+  _SearchUpdatePageState createState() => _SearchUpdatePageState();
+}
 
+class _SearchUpdatePageState extends State<SearchUpdatePage> {
+  TextEditingController searchController = TextEditingController();
+  SearchModel _dataFromAPI = SearchModel();
   final dio = Dio();
-  void getCustomer(BuildContext context) async {
-    final response =
-        await dio.get('http://10.0.2.2:8080/api/customer/getCustomer');
-    print(response);
+  var data = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    searchModels =
+        await getSearchModel(); // Assign the fetched searchModels to the list
+    print(searchModels);
+    // Now you can use the searchModels list as needed
+  }
+
+  Future<List<SearchModel>> getSearchModel() async {
+    final response = await dio.get(
+      'http://10.0.2.2:8080/api/cars/getCars',
+    );
+
+    // Check if response.data is a List<dynamic>
+    if (response.data is! List<dynamic>) {
+      // Handle the unexpected data format
+      throw Exception("Expected List<dynamic>, but got something else.");
+    }
+
+    // Convert the JSON array to a List of Maps
+    List<Map<String, dynamic>> responseDataList =
+        List<Map<String, dynamic>>.from(response.data);
+
+    // Map each item in the list to a SearchModel instance
+    List<SearchModel> searchModels =
+        responseDataList.map((map) => SearchModel.fromJson(map)).toList();
+
+    return searchModels;
+  }
+
+  void updateList(String value) {
+    setState(() {
+      searchModels = searchModels
+          .where((element) =>
+              element.CarID.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
   }
 
   @override
-  Widget build(BuildContext) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Search'),
       ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              onChanged: (value) => updateList(value),
+              style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                  hintText: "Enter car ID",
+                  suffixIcon: Icon(Icons.search),
+                  prefixIconColor: Colors.black,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)))),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Search for a car",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: searchModels.length,
+                itemBuilder: (context, index) => ListTile(
+                  title: Text(
+                    searchModels[index]
+                        .CarID, // Use searchModels instead of searchList
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    '${searchModels[index].CarID}', // Use searchModels instead of searchList
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
-
-  // Widget build(BuildContext context) {
-  //   getCustomer(context);
-  //   return SafeArea(
-  //       top: false,
-  //       bottom: false,
-  //       left: false,
-  //       right: false,
-  //       child: Scaffold(
-  //           backgroundColor: appTheme.blue900,
-  //           body: Container(
-  //               width: double.maxFinite,
-  //               decoration: AppDecoration.fillBlue,
-  //               child: Column(children: [
-  //                 SizedBox(height: 10.v),
-  //                 Expanded(
-  //                     child: SingleChildScrollView(
-  //                         child: Column(children: [
-  //                   // _buildHeadRow(context),
-  //                   SizedBox(height: 31.v),
-  //                   Padding(
-  //                       padding: EdgeInsets.only(left: 44.h, right: 43.h),
-  //                       child: CustomSearchView(
-  //                           controller: searchController, hintText: "CAR ID")),
-  //                   SizedBox(height: 42.v),
-  //                   Container(
-  //                       height: 900.h,
-  //                       padding: EdgeInsets.symmetric(
-  //                           horizontal: 25.h, vertical: 22.v),
-  //                       decoration: AppDecoration.fillWhiteA.copyWith(
-  //                         borderRadius: BorderRadius.only(
-  //                           topLeft: Radius.circular(20.0),
-  //                           topRight: Radius.circular(20.0),
-  //                           bottomLeft: Radius.circular(0.0),
-  //                           bottomRight: Radius.circular(0.0),
-  //                         ),
-  //                       ),
-  //                       child: Column(
-  //                           mainAxisSize: MainAxisSize.min,
-  //                           crossAxisAlignment: CrossAxisAlignment.start,
-  //                           children: [
-  //                             Padding(
-  //                                 padding: EdgeInsets.only(left: 10.h),
-  //                                 child: Text("Recent",
-  //                                     style: theme.textTheme.headlineLarge)),
-  //                             SizedBox(height: 33.v),
-  //                             _buildUserList(context),
-  //                             SizedBox(height: 33.v)
-  //                           ])),
-  //                   SizedBox(height: 53.v),
-  //                 ])))
-  //               ]))));
-  // }
-
-  /// Section Widget
-  // Widget _buildUserList(BuildContext context) {
-  //   return Padding(
-  //       padding: EdgeInsets.only(right: 4.h),
-  //       child: ListView.separated(
-  //           physics: NeverScrollableScrollPhysics(),
-  //           shrinkWrap: true,
-  //           separatorBuilder: (context, index) {
-  //             return Padding(
-  //                 padding: EdgeInsets.symmetric(vertical: 8.0.v),
-  //                 child: SizedBox(
-  //                     width: 376.h,
-  //                     child: Divider(
-  //                         height: 1.v,
-  //                         thickness: 1.v,
-  //                         color: appTheme.gray100)));
-  //           },
-  //           itemCount: 2,
-  //           itemBuilder: (context, index) {
-  //             String userName = "Anthony Edward Stark";
-  //             return UserlistItemWidget(onTapTxtUserName: () {
-  //               onTapTxtUserName(context, userName);
-  //             });
-  //           }));
-  // }
-
-  /// Navigates to the data1UpdateScreen when the action is triggered.
-  // void onTapTxtUserName(BuildContext context, String userName) {
-  //   if (userName == "Anthony Edward Stark") {
-  //     Navigator.push(
-  //       context,
-  //       MaterialPageRoute(builder: (context) => Data1UpdateScreen()),
-  //     );
-  //   } else {
-  //     print("Invalid username: $userName");
-  //   }
-  // }
 }
