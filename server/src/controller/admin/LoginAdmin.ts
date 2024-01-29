500a086bcdeb02febb11503f698913ea78cb6a03
Narutchai <koonfluk2015@gmail.com>
2024-01-29 16:34:35 +0700

    update loginAdmin

---
+++ server/src/controller/admin/LoginAdmin.ts
@@ -0,0 +1,38 @@
+import { Request,Response } from "express";
+import { conn,Connect } from "../../server";
+import { comparePassword } from "../../utils/ManagePassWord";
+import jwt from "jsonwebtoken";
+
+
+export const loginAdmin = async (req:Request,res:Response) => {
+    try {
+        const secret = process.env.JWT_SECRET;
+        await Connect();
+        const {Email,Password} = req.body;
+        const findAdmin:any = await conn?.query(`SELECT * FROM Admin WHERE Email = ?`,[Email]);
+        if (!findAdmin) {
+            return res.status(404).json({
+                status: "fail",
+                message: "Admin not found",
+            });
+        }
+        const Admin = findAdmin[0];
+        const compare = await comparePassword(Password,Admin[0].Password);
+        if (!compare) {
+            return res.status(404).json({
+                status: "fail",
+                message: "Password is not correct",
+            });
+        }
+        const token = jwt.sign({id:Admin[0].id},secret as string,{expiresIn: "1d"});
+        res.cookie("token",token,{expires: new Date(Date.now() + 86400000),httpOnly: true});
+        return res.status(200).json({
+            status: "success",
+            message: "Login success",
+            token,
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}    
--- server/src/routers/Admin.ts
+++ server/src/routers/Admin.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { CreateAdmin } from '../controller/admin/CreateAdmin';
+import { loginAdmin } from '../controller/admin/LoginAdmin';
 
 const router = express.Router();
 
 router.post('/createadmin',CreateAdmin);
+router.post('/loginadmin',loginAdmin);
 
 
 
