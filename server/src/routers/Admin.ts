3297ad4cf199bc1240d209693a9b795c77bdb6a8
Narutchai <koonfluk2015@gmail.com>
2024-02-05 18:20:53 +0700

    update

--- server/src/routers/Admin.ts
+++ server/src/routers/Admin.ts
@@ -5,6 +5,7 @@ import { getAdminByID } from '../controller/admin/GetAdminByID';
 import { getSurveyorByID } from '../controller/admin/Surveyor/getSurveyorByID';
 import { getCaseBySurveyorID } from '../controller/admin/Case/getCaseBySurveyorID';
 import { getCaseByCaseID } from '../controller/admin/Case/getCaseByCaseID';
+import { EditSurveyor } from '../controller/admin/Surveyor/EditSurveyor';
 
 const router = express.Router();
 
@@ -14,6 +15,7 @@ router.get('/getadmin',getAdminByID);
 router.get('/getsurveyor/:id',getSurveyorByID);
 router.get('/getCaseBySurveyorID/:surveyorID',getCaseBySurveyorID);
 router.get('/getCaseByCaseID/:caseId',getCaseByCaseID);
+router.put('/updateSurveyor/:id',EditSurveyor);
 
 
 
