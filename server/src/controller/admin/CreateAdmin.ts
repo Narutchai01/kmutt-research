0c283d17e7bcdf89c2153db4054a1b62130754a3
Narutchai <koonfluk2015@gmail.com>
2024-02-09 21:48:51 +0700

    update

---
+++ server/src/controller/admin/CreateAdmin.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { conn,Connect } from "../../server";
+import { upLoadeImageAdmin } from "../../utils/UploadImage";
+import { hashPassword } from "../../utils/ManagePassWord";
+
+export const CreateAdmin = async (req: Request, res: Response) => {
+  try {
+    const { First_name, Last_name, Birth_date, Phone_number, Email, Password } =
+      req.body;
+    const Images = req.files;
+    const sql = `INSERT INTO Admin (First_name, Last_name, Birth_date, Phone_number, Email, Password, Image) VALUES (?,?,?,?,?,?,?)`
+    const ImageURL = await Promise.all(
+      (Images as any[]).map(async (file: any) => {
+        const url = await upLoadeImageAdmin(file.buffer);
+        return url;
+      })
+    );
+    const DataAdmin = {
+        First_name,
+        Last_name,
+        Birth_date,
+        Phone_number,
+        Email,
+        Password : await hashPassword(Password),
+        ImageURL,
+    };
+    
+    await Connect();
+    await conn?.query(sql,[DataAdmin.First_name,DataAdmin.Last_name,DataAdmin.Birth_date,DataAdmin.Phone_number,DataAdmin.Email,DataAdmin.Password,DataAdmin.ImageURL[0]])
+    res.status(200).json({
+        status: "success",
+        data: DataAdmin,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
