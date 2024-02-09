2bc2519af99be1b0f9b13d824901c580c35dbb93
Narutchai <koonfluk2015@gmail.com>
2024-02-09 23:20:09 +0700

    updatea

---
+++ server/dist/routers/Admin.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const CreateAdmin_1 = require("../controller/admin/CreateAdmin");
+const LoginAdmin_1 = require("../controller/admin/LoginAdmin");
+const GetAdminByID_1 = require("../controller/admin/GetAdminByID");
+const getSurveyorByID_1 = require("../controller/admin/Surveyor/getSurveyorByID");
+const getCaseBySurveyorID_1 = require("../controller/admin/Case/getCaseBySurveyorID");
+const getCaseByCaseID_1 = require("../controller/admin/Case/getCaseByCaseID");
+const router = express_1.default.Router();
+router.post('/createadmin', CreateAdmin_1.CreateAdmin);
+router.post('/loginadmin', LoginAdmin_1.loginAdmin);
+router.get('/getadmin', GetAdminByID_1.getAdminByID);
+router.get('/getsurveyor/:id', getSurveyorByID_1.getSurveyorByID);
+router.get('/getCaseBySurveyorID/:surveyorID', getCaseBySurveyorID_1.getCaseBySurveyorID);
+router.get('/getCaseByCaseID/:caseId', getCaseByCaseID_1.getCaseByCaseID);
+exports.default = router;
--- server/dist/routers/Insurance.js
+++ server/dist/routers/Insurance.js
@@ -5,6 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const addInsurance_1 = require("../controller/Insurance/addInsurance");
+// import { getInsurance } from "../controller/Insurance/GetInsurance";
 const router = express_1.default.Router();
 router.post("/addInsurance", addInsurance_1.addInsurance);
+// router.get("/getInsurance", getInsurance);
 exports.default = router;
